894b65b37c75e2ee8383d1c48db63e1559037de0
ali m alghanay <a.al-ghanay@uot.edu.ly>
2024-04-13 06:40:13 +0200

    tests is on the way

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -11,8 +11,8 @@ class SudokuSolver {
   }
 
   validate(puzzleString) {
-    console.log(puzzleString);
-    if(puzzleString.length === 0) throw new Error("Required field missing");
+    
+    if(puzzleString.length === 0 || !puzzleString) throw new Error("Required field missing");
     if (puzzleString.length !== 81) throw new Error("Expected puzzle to be 81 characters long");
     if (!/^[1-9\.]+$/.test(puzzleString)) throw new Error("Invalid characters in puzzle");
     return true;
